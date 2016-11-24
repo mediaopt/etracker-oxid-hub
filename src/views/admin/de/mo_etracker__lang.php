b58d2d3d8fa8eafdc916de0fb56d4f37a03f2fde
Andre Moelle <andre.moelle@mediaopt.de>
2016-11-24 17:27:14 +0100

    Release v2.1.2.

--- src/views/admin/de/mo_etracker__lang.php
+++ src/views/admin/de/mo_etracker__lang.php
@@ -11,14 +11,13 @@ $aLang = [
 
     //labels and help-content for admin-area
     'SHOP_MODULE_GROUP_mo_etracker__config' => 'etracker Konfiguration',
-    'SHOP_MODULE_mo_etracker__securecode' => 'Sicherheitscode',
-    'HELP_SHOP_MODULE_mo_etracker__securecode' => 'Sicherheitscode eingeben',
-    'SHOP_MODULE_mo_etracker__securekey' => 'Sicherheitsschlüssel',
-    'HELP_SHOP_MODULE_mo_etracker__securekey' => 'Sicherheitsschlüssel eingeben',
+    'SHOP_MODULE_mo_etracker__securecode' => 'etracker Account-Schlüssel 1 (ehemals Sicherheitscode)',
+    'SHOP_MODULE_mo_etracker__securekey' => 'etracker API-Schlüssel (ehemals Sicherheitsschlüssel)',
     'SHOP_MODULE_mo_etracker__sechannel' => 'Suchmaschinenkanal',
-    'HELP_SHOP_MODULE_mo_etracker__sechannel' => 'Suchmaschinenkanal eingeben',
     'SHOP_MODULE_mo_etracker__root' => 'Wurzel',
     'HELP_SHOP_MODULE_mo_etracker__root' => 'Falls Sie einen Präfix für Ihre Seitennamen anlegen möchten, geben Sie diesen bitte hier an',
     'SHOP_MODULE_mo_etracker__debug' => 'Debug-Modus',
-    'HELP_SHOP_MODULE_mo_etracker__debug' => 'Im Debug-Modus werden die erzeugten Etracker-Daten in einem separaten Fenster im Frontend dargestellt, sobald Sie sich dort als Administrator angemeldet haben. Im Debug-Modus werden keine Daten an Etracker übermittelt.',
+    'HELP_SHOP_MODULE_mo_etracker__debug' => 'Im Debug-Modus werden keine Daten an Etracker übermittelt. ' .
+        'Die erzeugten etracker-Daten werden in einem separaten Fenster im Frontend und auf der JavaScript-Konsole dargestellt.'  .
+        'Für Ersteres muss man als Administrator angemeldet sein.',
 ];
--- src/views/admin/en/mo_etracker__lang.php
+++ src/views/admin/en/mo_etracker__lang.php
@@ -12,15 +12,14 @@ $aLang = [
 
     // Labels and help content for backend
     'SHOP_MODULE_GROUP_mo_etracker__config' => 'etracker Configuration',
-    'SHOP_MODULE_mo_etracker__securecode' => 'Securitycode',
-    'HELP_SHOP_MODULE_mo_etracker__securecode' => 'Enter securitycode',
-    'SHOP_MODULE_mo_etracker__securekey' => 'Securitykey',
-    'HELP_SHOP_MODULE_mo_etracker__securekey' => 'Enter securitykey',
-    'SHOP_MODULE_mo_etracker__sechannel' => 'SE-channel',
-    'HELP_SHOP_MODULE_mo_etracker__sechannel' => 'Enter search engine channel',
+    'SHOP_MODULE_mo_etracker__securecode' => 'etracker Account Key 1 (formerly: security code)',
+    'SHOP_MODULE_mo_etracker__securekey' => 'etracker API Key (formerly: security key)',
+    'SHOP_MODULE_mo_etracker__sechannel' => 'Search engine channel',
     'SHOP_MODULE_mo_etracker__root' => 'Root',
     'HELP_SHOP_MODULE_mo_etracker__root' => 'Enter a text here if you want to add a prefix to all of your pagenames',
     'HELP_SHOP_MODULE_mo_etracker__debug' => 'Debug-Mode',
-    'mo_etracker___HELP_DEBUG' => 'All generated etracker-data will be displayed in a separate area in frontend when logged in as administrator. No data is being send to etracker.',
+    'mo_etracker___HELP_DEBUG' => 'No data is being send to etracker. ' .
+        'All generated etracker-data will be displayed in a separate area and the JavaScript console. ' .
+        'The former functionality requires being logged in as administrator.',
 ];
 
