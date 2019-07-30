154e293e3749eaa80b23a0369f3dd04eaabf53d2
mbe <martin.beckmann@mediaopt.de>
2019-07-30 14:41:17 +0200

    #11422 language files in utf 8

--- src/modules/mo/etracker/views/admin/de/mo_etracker__lang.php
+++ src/modules/mo/etracker/views/admin/de/mo_etracker__lang.php
@@ -2,22 +2,22 @@
 $sLangName = 'Deutsch';
 
 $aLang = [
-    'charset' => 'ISO-8859-1',
+    'charset' => 'UTF-8',
 
     'tbclcategory_etracker'                 => 'etracker',
     'MO_ETRACKER__CATEGORY'                 => 'etracker-Kategorie',
-    'MO_ETRACKER__CATEGORY_HELP'            => 'Dieser Bezeichner wird statt des Kategorienamens an etracker übermittelt.',
+    'MO_ETRACKER__CATEGORY_HELP'            => 'Dieser Bezeichner wird statt des Kategorienamens an etracker Ã¼bermittelt.',
     'MO_ETRACKER__SAVE'                     => 'Speichern',
 
     //labels and help-content for admin-area
     'SHOP_MODULE_GROUP_mo_etracker__config' => 'etracker Konfiguration',
-    'SHOP_MODULE_mo_etracker__securecode'   => 'etracker Account-Schlüssel 1 (ehemals Sicherheitscode)',
-    'SHOP_MODULE_mo_etracker__securekey'    => 'etracker API-Schlüssel (ehemals Sicherheitsschlüssel)',
+    'SHOP_MODULE_mo_etracker__securecode'   => 'etracker Account-SchlÃ¼ssel 1 (ehemals Sicherheitscode)',
+    'SHOP_MODULE_mo_etracker__securekey'    => 'etracker API-SchlÃ¼ssel (ehemals SicherheitsschlÃ¼ssel)',
     'SHOP_MODULE_mo_etracker__sechannel'    => 'Suchmaschinenkanal',
     'SHOP_MODULE_mo_etracker__root'         => 'Wurzel',
-    'HELP_SHOP_MODULE_mo_etracker__root'    => 'Falls Sie einen Präfix für Ihre Seitennamen anlegen möchten, geben Sie diesen bitte hier an',
+    'HELP_SHOP_MODULE_mo_etracker__root'    => 'Falls Sie einen PrÃ¤fix fÃ¼r Ihre Seitennamen anlegen mÃ¶chten, geben Sie diesen bitte hier an',
     'SHOP_MODULE_mo_etracker__debug'        => 'Debug-Modus',
-    'HELP_SHOP_MODULE_mo_etracker__debug'   => 'Im Debug-Modus werden keine Daten an Etracker übermittelt. ' .
+    'HELP_SHOP_MODULE_mo_etracker__debug'   => 'Im Debug-Modus werden keine Daten an Etracker Ã¼bermittelt. ' .
         'Die erzeugten etracker-Daten werden in einem separaten Fenster im Frontend und auf der JavaScript-Konsole dargestellt.'  .
-        'Für Ersteres muss man als Administrator angemeldet sein.',
+        'FÃ¼r Ersteres muss man als Administrator angemeldet sein.',
 ];
