0255f9f01a45885fceaf335cce4aa96d78b2727b
yangchao <yangchao.liu@mediaopt.de>
2021-12-09 09:07:36 +0100

    #23706 fix the autoload bug

--- src/modules/mo/etracker/bootstrap.php
+++ src/modules/mo/etracker/bootstrap.php
@@ -1,12 +1,5 @@
 <?php
 
-//spl_autoload_register(function ($class) {
-//    $prefix = 'mo_etracker';
-//    $suffix = 'Event';
-//    if (strpos($class, $prefix) !== false && strpos($class, $suffix) !== false) {
-//        require_once OX_BASE_PATH . 'modules/mo/etracker/Event/' . $class . '.php';
-//    }
-//});
 spl_autoload_register(function ($class) {
     if ($class === 'mo_etracker__event') {
         require_once OX_BASE_PATH . 'modules/mo/etracker/Event.php';
